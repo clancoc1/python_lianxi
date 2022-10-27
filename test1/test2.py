0029592f3ea77d7fc33f5b6d86cd41612a32cd74
20221027 <20221027@163.com>
2022-10-27 17:12:56 +0800

    first commit

---
+++ test1/test2.py
@@ -0,0 +1,18 @@
+from multiprocessing import Process
+# def fun():
+#     print("hello")
+#
+# if __name__=="__main__":
+#     p=Process(target=fun,args=())
+#     p.start()
+
+class my_process(Process):
+    def __init__(self,name):
+        super().__init__()
+        self.name=name
+    def run(self):
+        print("the name id %s"%self.name)
+
+if __name__=="__main__":
+    p=my_process("线程1")
+    p.start()
